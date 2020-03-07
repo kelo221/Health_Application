260a01b69940a8284b4dbd41d54726e27b5e68bb
villekum <ville.kumpulainen@metropolia.fi>
2020-03-08 01:11:45 +0200

    Added content to the settings tab. Made folders for classes.

--- app/src/main/java/com/terveyssovellus/softa/fragments/SettingsFragment.java
+++ app/src/main/java/com/terveyssovellus/softa/fragments/SettingsFragment.java
@@ -8,16 +8,13 @@ import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
-import android.widget.Toast;
 
 import androidx.fragment.app.Fragment;
 
-import com.terveyssovellus.softa.CurrentProgram;
-import com.terveyssovellus.softa.LanguageSelection;
+import com.terveyssovellus.softa.fragments.content.LanguageSelection;
 import com.terveyssovellus.softa.R;
 
 import java.util.ArrayList;
-import java.util.List;
 
 public class SettingsFragment extends Fragment {
 
--- app/src/main/java/com/terveyssovellus/softa/fragments/content/LanguageSelection.java
+++ app/src/main/java/com/terveyssovellus/softa/fragments/content/LanguageSelection.java
@@ -1,8 +1,10 @@
-package com.terveyssovellus.softa;
+package com.terveyssovellus.softa.fragments.content;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+
+import com.terveyssovellus.softa.R;
 
 public class LanguageSelection extends AppCompatActivity {
 
