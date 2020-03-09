d55d55124ce83b658c0e0992e9e8f81837a1f58f
jerelam <jere.lampola@live.fi>
2020-03-09 12:05:00 +0200

    Language selection fixed

--- app/src/main/java/com/terveyssovellus/softa/fragments/content/LanguageSelection.java
+++ app/src/main/java/com/terveyssovellus/softa/fragments/content/LanguageSelection.java
@@ -7,7 +7,6 @@ import android.content.res.Resources;
 import android.os.Bundle;
 import android.util.DisplayMetrics;
 import android.view.View;
-import android.widget.ImageButton;
 import com.terveyssovellus.softa.MainActivity;
 import com.terveyssovellus.softa.R;
 import java.util.Locale;
@@ -18,27 +17,10 @@ public class LanguageSelection extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_language_selection);
-
-        ImageButton langButtonEN = findViewById(R.id.lan_eng);
-        ImageButton langButtonFI = findViewById(R.id.lan_fi);
-        ImageButton langButtonSV = findViewById(R.id.lan_swe);
-
-        langButtonEN.setOnClickListener(new View.OnClickListener(){
-            @Override
-            public void onClick(View caller) {
-                changeLang((String)caller.getTag());
-            }
-        });
-
-        langButtonFI.setOnClickListener(new View.OnClickListener(){
-            @Override
-            public void onClick(View caller) {
-                changeLang((String)caller.getTag());
-            }
-        });
     }
 
-    public void changeLang(String lang){
+    public void changeLanguage(View caller){
+        String lang = (String)caller.getTag();
         Locale locale = new Locale(lang);
         Resources res = getResources();
         DisplayMetrics dm = res.getDisplayMetrics();
