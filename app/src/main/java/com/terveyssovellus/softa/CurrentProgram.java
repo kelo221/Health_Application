1de9fdbb03ef6ea61a2dad783c92587dc9c4a963
villekum <ville.kumpulainen@metropolia.fi>
2020-03-10 22:05:12 +0200

    Revert "Made possible to dynamically change plans"
    This reverts commit a39988b2b95b31cdff29c0f9883ee7a3d3c65752.

--- app/src/main/java/com/terveyssovellus/softa/CurrentProgram.java
+++ app/src/main/java/com/terveyssovellus/softa/CurrentProgram.java
@@ -2,47 +2,13 @@ package com.terveyssovellus.softa;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.widget.Toast;
-
-import com.terveyssovellus.softa.fragments.AddFragment;
-import com.terveyssovellus.softa.fragments.content.LanguageSelection;
-import com.terveyssovellus.softa.fragments.content.Licences;
 
 public class CurrentProgram extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-        String layout = "default";
-        int chooseProgram;
-        try {
-            layout = getIntent().getStringExtra("layout_selector");
-        } catch (Exception e){
-            e.printStackTrace();
-        }
-
-        Toast.makeText(this,layout, Toast.LENGTH_SHORT).show();
-        chooseProgram = Integer.valueOf(layout);
-
-
-
-        switch(chooseProgram) {
-            case 222:
-                setContentView(R.layout.activity_nasal_polyops);
-                break;
-            case 444:
-                setContentView(R.layout.activity_septoplasty);
-                break;
-            default:
-                setContentView(R.layout.activity_nasal_polyops);
-                break;
-        }
-
-
-
-
+        setContentView(R.layout.activity_nasal_polyops);
     }
 }
