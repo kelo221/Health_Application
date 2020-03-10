b04e611053611d0ce649a621ef715364d156c1f5
villekum <ville.kumpulainen@metropolia.fi>
2020-03-10 12:46:48 +0200

    debug for XML

--- app/src/main/java/com/terveyssovellus/softa/fragments/ListFragment.java
+++ app/src/main/java/com/terveyssovellus/softa/fragments/ListFragment.java
@@ -20,6 +20,7 @@ public class ListFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState){
-        return inflater.inflate(R.layout.fragment_list, container, false);
+        //return inflater.inflate(R.layout.fragment_list, container, false);
+        return inflater.inflate(R.layout.activity_nasal_polyops, container, false);
     }
 }
