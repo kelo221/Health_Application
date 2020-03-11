996189e64febb9ef99b5943cc7782a3525d3d9d9
Johanna <johanna.vasala@gmail.com>
2020-03-11 16:37:07 +0200

    Added possibility to call to emergency number

--- app/src/main/java/com/terveyssovellus/softa/fragments/HelpFragment.java
+++ app/src/main/java/com/terveyssovellus/softa/fragments/HelpFragment.java
@@ -1,17 +1,24 @@
 package com.terveyssovellus.softa.fragments;
 
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
 import android.os.Bundle;
+
+import androidx.core.app.ActivityCompat;
 import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import com.terveyssovellus.softa.R;
 
-public class HelpFragment extends Fragment {
+public class HelpFragment extends Fragment implements View.OnClickListener {
 
     public HelpFragment(){}
-
+    final String alarmnumber = "tel:00030";
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,9 +27,27 @@ public class HelpFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState){
-        return inflater.inflate(R.layout.fragment_help, container, false);
+
+
+        View view = inflater.inflate(R.layout.fragment_help, container, false);
+
+        Button button = (Button) view.findViewById(R.id.buttonCall);
+        button.setOnClickListener(this);
 
 
 
+
+        return view;
     }
-}
+    @Override
+    public void onClick(View arg0) {
+            Intent callIntent = new Intent(Intent.ACTION_CALL);
+            callIntent.setData(Uri.parse( alarmnumber));
+
+            if (ActivityCompat.checkSelfPermission(getContext(),
+                    Manifest.permission.CALL_PHONE) != PackageManager.PERMISSION_GRANTED) {
+                return;
+            }
+            startActivity(callIntent);
+        }
+};
